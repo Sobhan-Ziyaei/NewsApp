bdb36301abc633fd1a72ac7af4536fee78c07da9
SohanZiyaei <sobhan.ziaee73@gmail.com>
2023-03-27 11:39:45 +0430

    add json and retrofit library and edit news class

--- app/src/main/java/com/newsapp/MainActivity.kt
+++ app/src/main/java/com/newsapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    private val newsList = mutableListOf<News>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialRecView()
+        //LifecycleScope is optimize because it is run until activity does not destroy so fill not the RAM
+        lifecycleScope.launch {
+
+        }
+    }
+
+    private fun initialRecView() {
+        val recView = findViewById<RecyclerView>(R.id.recView)
+        val newsAdapter = NewsAdaptor(newsList)
+        recView.adapter = newsAdapter
+        recView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private suspend fun networkCall(): String {
+        delay(2000)
+        return "ali"
     }
 }
